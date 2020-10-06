a4cd971282bee4a49f08c9efa88acc5d9d083fa5
latrac_y <latrac_y@etna-alternance.net>
2020-10-07 00:48:05 +0200

    add base url in .env

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import App from './App';
+import axios from 'axios';
 import {name as appName} from './app.json';
+import env from '.env'
 
+axios.defaults.baseURL = env.BASE_URL;
 AppRegistry.registerComponent(appName, () => App);
+
